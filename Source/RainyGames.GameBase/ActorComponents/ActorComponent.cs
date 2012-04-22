4437bd924f34c89c9a14f1a27a0ad0fa0ca6e4a9
Nick Pruehs <dev@npruehs.de>
2012-04-22 17:00:57 +0200

    - ADDED: Initial Game, Actor, Player and ActorComponent classes of the GameBase library.

---
+++ Source/RainyGames.GameBase/ActorComponents/ActorComponent.cs
@@ -0,0 +1,71 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ActorComponent.cs" company="Rainy Games">
+// Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RainyGames.GameBase.ActorComponents
+{
+    using System;
+
+    /// <summary>
+    /// Abstract base class of all actor components. Actor components can be
+    /// attached to actors in order to add further functionality, i.e. health
+    /// or attacks. They are ticked by their owning actors.
+    /// </summary>
+    public abstract class ActorComponent
+    {
+        #region Constants and Fields
+
+        /// <summary>
+        /// Actor this component is attached to.
+        /// </summary>
+        private Actor actor;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Constructs a new actor component.
+        /// </summary>
+        /// <param name="actor">
+        /// Actor the new component will be attached to.
+        /// </param>
+        public ActorComponent(Actor actor)
+        {
+            if (actor == null)
+            {
+                throw new ArgumentNullException("actor");
+            }
+
+            this.actor = actor;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Actor this component is attached to.
+        /// </summary>
+        public Actor Actor
+        {
+            get { return this.actor; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Allows this actor component to update itself.
+        /// </summary>
+        /// <param name="dt">
+        /// Time passed since the last tick.
+        /// </param>
+        public abstract void Update(float dt);
+
+        #endregion
+    }
+}
---
+++ Source/RainyGames.GameBase/Player.cs
@@ -0,0 +1,104 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Player.cs" company="Rainy Games">
+// Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RainyGames.GameBase
+{
+    using System;
+
+    /// <summary>
+    /// Player participating in a specific game. Players can be the owners of
+    /// actors, i.e. the units they build or the projectiles they fired.
+    /// </summary>
+    public class Player
+    {
+        #region Constants and Fields
+
+        /// <summary>
+        /// Game this player participates in.
+        /// </summary>
+        private Game game;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Constructs a new player to participate in the passed game,
+        /// assigning a default index and name.
+        /// </summary>
+        /// <param name="game">
+        /// Game the new player will participate in.
+        /// </param>
+        public Player(Game game)
+            : this(game, 0)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new player with the specified index to participate in
+        /// the passed game, assigning a default name.
+        /// </summary>
+        /// <param name="game">
+        /// Game the new player will participate in.
+        /// </param>
+        /// <param name="index">
+        /// Index of the player in the game.
+        /// </param>
+        public Player(Game game, int index)
+            : this(game, index, "Player " + index)
+        {
+        }
+
+        /// <summary>
+        /// Constructs a new player with the specified index and name to
+        /// participate in the passed game.
+        /// </summary>
+        /// <param name="game">
+        /// Game the new player will participate in.
+        /// </param>
+        /// <param name="index">
+        /// Index of the player in the game.
+        /// </param>
+        /// <param name="name">
+        /// Name of the new player.
+        /// </param>
+        public Player(Game game, int index, string name)
+        {
+            if (game == null)
+            {
+                throw new ArgumentNullException("game");
+            }
+
+            this.game = game;
+            this.PlayerIndex = index;
+            this.PlayerName = name;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Game this player participates in.
+        /// </summary>
+        public Game Game
+        {
+            get { return this.game; }
+        }
+
+        /// <summary>
+        /// Index of this player in the game he or she is participating in.
+        /// </summary>
+        public int PlayerIndex { get; set; }
+
+        /// <summary>
+        /// Name of this player.
+        /// </summary>
+        public string PlayerName { get; set; }
+
+        #endregion
+    }
+}
