b3bf88624fef05b7210a90da9792e3d0ea7fbfd3
Christian Oeing <oeing@gmx.at>
2012-05-03 16:17:27 +0200

    ADDED: Unity Common project.

---
+++ Source/RainyGames.Unity.Common/Source/Math/MathConversionExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RainyGames.Math.Algebra.Vectors;
+using UnityEngine;
+
+namespace RainyGames.Unity.Common.Math
+{
+    /// <summary>
+    ///   Contains extension methods to convert from/to unity/framework mathematical structures.
+    /// </summary>
+    public static class MathConversionExtensionMethods
+    {
+        public static Vector3 ToVector3(this Vector3F vector)
+        {
+            return new Vector3(vector.X, vector.Y, vector.Z);
+        }
+
+        public static Vector3 ToVector3(this Vector2F vector, float height = 0.0f)
+        {
+            return new Vector3(vector.X, height, vector.Y);
+        }
+
+        public static Vector3 ToVector3(this Vector2I vector, float height = 0.0f, float factor = 1.0f)
+        {
+            return new Vector3(vector.X * factor, height, vector.Y * factor);
+        }
+    }
+}
