d71bd0a9cf46288cee446b5dd799839bedae4104
Christian Oeing <oeing@gmx.at>
2012-07-02 14:27:54 +0200

    ADDED: Framework Xml - Added library containing stuff for xml serialization.

---
+++ Source/RainyGames.Xml/Source/Utils/XmlReaderExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace RainyGames.Xml.Utils
+{
+    using System.Xml;
+
+    /// <summary>
+    ///   Extension methods for the XmlReader.
+    /// </summary>
+    public static class XmlReaderExtensionMethods
+    {
+    }
+}
