449bf4c4eed817cd1a02c7fd00405cb59ad0f89c
Christian Oeing <oeing@gmx.at>
2012-07-02 14:26:35 +0200

    ADDED: Framework Collections - Library containing classes, extension methods, utils for collections.

---
+++ Source/RainyGames.Collections/Source/AttributeTables/AttributeTable.cs
@@ -0,0 +1,58 @@
+﻿namespace RainyGames.Collections.AttributeTables
+{
+    using System.Collections.Generic;
+
+    public class AttributeTable
+    {
+        /// <summary>
+        ///   Dictionary to store attributes.
+        /// </summary>
+        private readonly Dictionary<object, object> attributes = new Dictionary<object, object>();
+
+        /// <summary>
+        ///   Copy constructor.
+        /// </summary>
+        /// <param name="original">Attribute table to copy.</param>
+        public AttributeTable(AttributeTable original)
+        {
+            this.CopyAttributes(original);
+        }
+
+        public AttributeTable()
+        {
+        }
+
+        public void SetValue(object key, object value)
+        {
+            this.attributes[key] = value;
+        }
+
+        public void Remove(object key)
+        {
+            this.attributes.Remove(key);
+        }
+
+        public void Add(object key, object value)
+        {
+            this.attributes.Add(key, value);
+        }
+
+        private void CopyAttributes(AttributeTable original)
+        {
+            foreach (KeyValuePair<object, object> keyValuePair in original.attributes)
+            {
+                this.attributes.Add(keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+
+        protected bool ContainsKey(object id)
+        {
+            return this.attributes.ContainsKey(id);
+        }
+
+        protected bool TryGetValue(object id, out object value)
+        {
+            return this.attributes.TryGetValue(id, out value);
+        }
+    }
+}
---
+++ Source/RainyGames.Collections/Source/Utils/CollectionUtils.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CollectionUtils.cs" company="Rainy Games">
+//   Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RainyGames.Collections.Utils
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class CollectionUtils
+    {
+        #region Public Methods and Operators
+
+
+        /// <summary>
+        ///   Compares two sequences by comparing their items instead of their references. Also checks the case that
+        ///   both sequences are null. In this case the method returns true.
+        /// </summary>
+        /// <typeparam name = "T">Type of items inside sequences.</typeparam>
+        /// <param name = "sequence1">First sequence.</param>
+        /// <param name = "sequence2">Second sequence.</param>
+        /// <returns>
+        ///   True if the sequences are equal (i.e. the sequences contain equal items in the same order), else false.
+        /// </returns>
+        public static bool SequenceEqual<T>(IEnumerable<T> sequence1, IEnumerable<T> sequence2)
+        {
+            return sequence1 == sequence2 ||
+                   sequence1 != null && sequence2 != null && sequence1.SequenceEqual(sequence2);
+        }
+
+        /// <summary>
+        ///   Compares two sequences by comparing their items instead of their references. Also checks the case that
+        ///   both sequences are null. In this case the method returns true.
+        /// </summary>
+        /// <typeparam name = "T">Type of items inside sequences.</typeparam>
+        /// <param name = "sequence1">First sequence.</param>
+        /// <param name = "sequence2">Second sequence.</param>
+        /// <param name = "comparer">Comparer to check equality between two objects.</param>
+        /// <returns>
+        ///   True if the sequences are equal (i.e. the sequences contain equal items in the same order), else false.
+        /// </returns>
+        public static bool SequenceEqual<T>(
+            IEnumerable<T> sequence1, IEnumerable<T> sequence2, IEqualityComparer<T> comparer)
+        {
+            return sequence1 == sequence2 ||
+                   sequence1 != null && sequence2 != null && sequence1.SequenceEqual(sequence2, comparer);
+        }
+
+        #endregion
+    }
+}
