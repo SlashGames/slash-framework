d4a02aa5e48b30eadfac43e81e1fbb85874ebb90
Christian Oeing <oeing@gmx.at>
2012-07-03 13:34:47 +0200

    ADDED: Framework Tools - Added batch file for copying DLLs for a Unity project into the correct folder.

---
+++ Tools/Batch/PostbuildUnityLibraries.bat
@@ -0,0 +1,35 @@
+REM 1. Create a new, empty project which only use is to copy all dlls to Unity.
+REM    Make sure it uses .NET 3.5 to be compatible with Unity.
+REM
+REM 2. Reference all dll projects to this new project.
+REM
+REM 3. Put a copy of this batch file into the project folder (e.g. into a folder called Build).
+REM 
+REM 4. Put this line into the post-build event of your project 
+REM    (replace PATH_TO_UNITY_PROJECT by the path to the unity project folder):
+REM    "$(ProjectDir)Build\Postbuild.bat" PATH_TO_UNITY_PROJECT $(TargetDir)
+REM
+REM 5. The project will be compiled after all dll projects (as they are referenced by this project)
+REM    and the dlls will be copied into the target directory of the project.
+REM
+REM    Then the post-build event will call this batch file, which will copy all dlls from the
+REM    target directory into the plugins folder of your unity project except for unity dlls.
+
+@echo OFF
+
+REM Read arguments.
+set UNITY_PATH=%1
+set DLL_TARGET_DIR=%2
+
+REM Don't copy Unity dlls, so hide them.
+attrib +h UnityEngine.dll > NUL
+attrib +h UnityEditor.dll > NUL
+
+REM Copy all dlls to Unity plugins folder.
+set TARGET_FOLDER=%UNITY_PATH%Assets\Plugins\
+echo Copying "%DLL_TARGET_DIR%*.dll" to "%TARGET_FOLDER%"
+xcopy "%DLL_TARGET_DIR%*.dll" "%TARGET_FOLDER%" /d /y
+
+REM Show hidden files again.
+attrib -h UnityEngine.dll > NUL
+attrib -h UnityEditor.dll > NUL
