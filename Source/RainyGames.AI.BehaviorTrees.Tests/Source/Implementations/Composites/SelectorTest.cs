a95e9660004e55fb5ac783f6364e3108d03f0236
Christian Oeing <oeing@gmx.at>
2012-07-03 16:59:45 +0200

    ADDED: AI.BehaviorTrees.Tests - Added unit test project for AI.BehaviorTrees.

---
+++ Source/RainyGames.AI.BehaviorTrees.Tests/Source/Implementations/Composites/SelectorTest.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SelectorTest.cs" company="Rainy Games">
+//   Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RainyGames.AI.BehaviorTrees.Tests.Implementations.Composites
+{
+    using NUnit.Framework;
+
+    using RainyGames.AI.BehaviorTrees.Editor;
+    using RainyGames.AI.BehaviorTrees.Implementations.Composites;
+
+    public class SelectorTest
+    {
+        #region Public Methods and Operators
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void TestGenerateTaskDescription()
+        {
+            TaskDescription taskDescription = TaskDescription.Generate<Selector>();
+            Assert.IsNotNull(taskDescription);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/RainyGames.AI.BehaviorTrees.Tests/Source/Implementations/Composites/SequenceTest.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SequenceTest.cs" company="Rainy Games">
+//   Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RainyGames.AI.BehaviorTrees.Tests.Implementations.Composites
+{
+    using NUnit.Framework;
+
+    public class SequenceTest
+    {
+        #region Public Methods and Operators
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        #endregion
+    }
+}
