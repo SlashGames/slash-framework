2b5e7f1ef521365ecaf7c784fcfe769430a30290
Christian Oeing <oeing@gmx.at>
2012-07-06 21:01:21 +0200

    ADDED: Framework Reflection - Project containing utilities for reflection.

---
+++ Source/RainyGames.Reflection.Tests/Source/PropertyUtilsTest.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PropertyUtilsTest.cs" company="Rainy Games">
+//   Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RainyGames.Reflection.Tests
+{
+    using System.Reflection;
+
+    using NUnit.Framework;
+
+    public class PropertyUtilsTest
+    {
+        #region Public Methods and Operators
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void TestGetPropertyInfo()
+        {
+            PropertyInfo propertyInfo = PropertyUtils<TestClass>.GetPropertyInfo(obj => obj.TestProperty);
+            Assert.NotNull(propertyInfo);
+        }
+
+        #endregion
+
+        public class TestClass
+        {
+            #region Public Properties
+
+            public float TestProperty { get; set; }
+
+            #endregion
+        }
+    }
+}
