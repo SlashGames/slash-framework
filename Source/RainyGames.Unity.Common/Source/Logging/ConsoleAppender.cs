ceb0745cc2122b44530399af2d81fd302c5fb795
Christian Oeing <oeing@gmx.at>
2012-07-06 16:14:37 +0200

    ADDED: Framework Unity.Common - Added wrapper for log4net, so it can be removed from a webplayer build.

---
+++ Source/RainyGames.Unity.Common/Source/Logging/ConsoleAppender.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleAppender.cs" company="Rainy Games">
+//   Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RainyGames.Unity.Common.Logging
+{
+    using UnityEngine;
+
+    using log4net.Appender;
+    using log4net.Core;
+
+    public class ConsoleAppender : AppenderSkeleton
+    {
+        #region Methods
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            string message = this.RenderLoggingEvent(loggingEvent);
+
+            if (loggingEvent.Level == Level.Error)
+            {
+                Debug.LogError(message);
+            }
+            else if (loggingEvent.Level == Level.Warn)
+            {
+                Debug.LogWarning(message);
+            }
+            else
+            {
+                Debug.Log(message);
+            }
+        }
+
+        #endregion
+    }
+}
