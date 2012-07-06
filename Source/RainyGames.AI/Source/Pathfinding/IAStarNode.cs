aa4ae3b0c1a8b351caeffea96fc7a5cc551c9803
Nick Pruehs <dev@npruehs.de>
2012-07-06 14:24:41 +0200

    - ADDED: Implementation of the A* algorithm.

---
+++ Source/RainyGames.AI/Source/Pathfinding/IAStarNode.cs
@@ -0,0 +1,63 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IAStarNode.cs" company="Rainy Games">
+// Copyright (c) Rainy Games. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RainyGames.AI.Pathfinding
+{
+    using RainyGames.Collections.Graphs;
+
+    /// <summary>
+    /// Node used for the A* pathfinding algorithm.
+    /// </summary>
+    public interface IAStarNode : IGraphVertex
+    {
+        /// <summary>
+        /// Specifies whether this node is walkable or not.
+        /// </summary>
+        bool IsWalkable { get; }
+
+        /// <summary>
+        /// Previous node on the path to the finish.
+        /// </summary>
+        IAStarNode ParentNode { get; set; }
+
+        /// <summary>
+        /// F score of this node, computed by adding G and H.
+        /// </summary>
+        int F { get; set; }
+
+        /// <summary>
+        /// G score of this node, telling the movement cost needed
+        /// for travelling from the starting node to this one.
+        /// </summary>
+        int G { get; set; }
+
+        /// <summary>
+        /// H score of this node, telling the estimated movement cost
+        /// needed for travelling from this node to the finish.
+        /// </summary>
+        int H { get; set; }
+
+        /// <summary>
+        /// Whether this node has already been discovered and added to the
+        /// open list, or not.
+        /// </summary>
+        bool Discovered { get; set; }
+
+        /// <summary>
+        /// Whether this node has already been visited and moved to the
+        /// closed list, or not.
+        /// </summary>
+        bool Visited { get; set; }
+
+        /// <summary>
+        /// Returns the estimated, heuristic movement cost needed to get
+        /// from this node to the specified other one.
+        /// </summary>
+        /// <param name="target">Target node.</param>
+        /// <returns>Estimated movement cost.</returns>
+        int EstimateHeuristicMovementCost(IAStarNode target);
+    }
+}
