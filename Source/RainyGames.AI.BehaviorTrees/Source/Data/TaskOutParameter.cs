35c76be2bab79073c8cabba9d175844a33ad567d
Christian Oeing <oeing@gmx.at>
2012-08-26 16:23:38 +0200

    ADDED: Behavior Trees - Added TaskOutParameter class.

---
+++ Source/RainyGames.AI.BehaviorTrees/Source/Data/TaskOutParameter.cs
@@ -0,0 +1,22 @@
+namespace RainyGames.AI.BehaviorTrees.Data
+{
+    using RainyGames.AI.BehaviorTrees.Interfaces;
+
+    public class TaskOutParameter<T>
+    {
+        /// <summary>
+        ///   Key of blackboard attribute to store task parameter value to.
+        /// </summary>
+        public object BlackboardAttribute { get; set; }
+
+        /// <summary>
+        ///   Sets the task parameter value.
+        /// </summary>
+        /// <param name="agentData">Agent data to set task parameter to.</param>
+        /// <param name="value">Value of task parameter.</param>
+        public void SetValue(IAgentData agentData, T value)
+        {
+            agentData.Blackboard.SetValue(this.BlackboardAttribute, value);
+        }
+    }
+}
