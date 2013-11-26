89fa5ac82e2b0c3e54ced7602b0146190520c1cf
Christian Oeing <christian.oeing@slashgames.org>
2013-11-26 19:08:46 +0100

    CHANGED: Blueprint Editor - Moved common logic into base InspectorControl class.

---
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Inspectors/InspectorControl.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InspectorControl.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BlueprintEditor.Inspectors
+{
+    using System.Windows.Controls;
+
+    using Slash.GameBase.Attributes;
+
+    public class InspectorControl : UserControl, IInspectorControl
+    {
+        #region Fields
+
+        /// <summary>
+        ///   Data context of inspector.
+        /// </summary>
+        private InspectorPropertyData dataContext;
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        ///   Raised when value of the inspector control changed.
+        /// </summary>
+        public event InspectorControlValueChangedDelegate ValueChanged;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Initializes the control with the inspector property it is for and the current value.
+        /// </summary>
+        /// <param name="inspectorProperty">Inspector property the control is for.</param>
+        /// <param name="currentValue">Current value.</param>
+        public void Init(InspectorPropertyAttribute inspectorProperty, object currentValue)
+        {
+            // Setup data context of control.
+            this.dataContext = new InspectorPropertyData { InspectorProperty = inspectorProperty, Value = currentValue };
+            this.dataContext.ValueChanged += this.OnValueChanged;
+            this.DataContext = this.dataContext;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnValueChanged(InspectorPropertyAttribute inspectorProperty, object newValue, object oldValue)
+        {
+            InspectorControlValueChangedDelegate handler = this.ValueChanged;
+            if (handler != null)
+            {
+                handler(inspectorProperty, newValue, oldValue);
+            }
+        }
+
+        #endregion
+    }
+}
--- Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Inspectors/TextBoxInspector.xaml.cs
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Inspectors/TextBoxInspector.xaml.cs
@@ -6,22 +6,11 @@
 
 namespace BlueprintEditor.Inspectors
 {
-    using Slash.GameBase.Attributes;
-
     /// <summary>
     ///   Inspector to edit a value in a textbox.
     /// </summary>
-    public sealed partial class TextBoxInspector : IInspectorControl
+    public partial class TextBoxInspector
     {
-        #region Fields
-
-        /// <summary>
-        ///   Data context of inspector.
-        /// </summary>
-        private InspectorPropertyData dataContext;
-
-        #endregion
-
         #region Constructors and Destructors
 
         /// <summary>
@@ -30,45 +19,6 @@ namespace BlueprintEditor.Inspectors
         public TextBoxInspector()
         {
             this.InitializeComponent();
-        }
-
-        #endregion
-
-        #region Public Events
-
-        /// <summary>
-        ///   Raised when value of the inspector control changed.
-        /// </summary>
-        public event InspectorControlValueChangedDelegate ValueChanged;
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        ///   Initializes the control with the inspector property it is for and the current value.
-        /// </summary>
-        /// <param name="inspectorProperty">Inspector property the control is for.</param>
-        /// <param name="currentValue">Current value.</param>
-        public void Init(InspectorPropertyAttribute inspectorProperty, object currentValue)
-        {
-            // Setup data context of control.
-            this.dataContext = new InspectorPropertyData { InspectorProperty = inspectorProperty, Value = currentValue };
-            this.dataContext.ValueChanged += this.OnValueChanged;
-            this.DataContext = this.dataContext;
-        }
-
-        #endregion
-
-        #region Methods
-
-        private void OnValueChanged(InspectorPropertyAttribute inspectorProperty, object newValue, object oldValue)
-        {
-            InspectorControlValueChangedDelegate handler = this.ValueChanged;
-            if (handler != null)
-            {
-                handler(inspectorProperty, newValue, oldValue);
-            }
         }
 
         #endregion
