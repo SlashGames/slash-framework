05c8bceda6541efb6e77e94cf37c91a53c001cf7
Christian Oeing <christian.oeing@slashgames.org>
2013-11-27 14:28:51 +0100

    ADDED: Blueprint Editor - Added error box for textbox inspector to show a validation error.

---
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Converters/BooleanOrConverter.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BooleanOrConverter.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BlueprintEditor.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows.Data;
+
+    public class BooleanOrConverter : IMultiValueConverter
+    {
+        #region Public Methods and Operators
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Any(value => (bool)value);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
