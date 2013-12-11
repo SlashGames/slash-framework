e27956bd488d2d73fafeb3c6453977d1fe16f789
Christian Oeing <christian.oeing@slashgames.org>
2013-12-11 16:37:52 +0100

    ADDED: Blueprint Editor - Listing class names of components in component list boxes instead of full name.

---
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Converters/TypeToClassNameConverter.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeToClassNameConverter.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace BlueprintEditor.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class TypeToClassNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Type)value).Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
