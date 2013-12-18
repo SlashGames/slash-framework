fbd6c9f54d09cc1eef85b3c21c95b7b58077bb68
Christian Oeing <christian.oeing@slashgames.org>
2013-12-18 17:08:49 +0100

    ADDED: Blueprint Editor - Added UpDown controls for integer and float attributes.

---
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Inspectors/IntegerUpDownInspector.xaml.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TextBoxInspector.xaml.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BlueprintEditor.Inspectors
+{
+    /// <summary>
+    ///   Inspector to edit a value in a integer up down control.
+    /// </summary>
+    public partial class IntegerUpDownInspector
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///   Constructor.
+        /// </summary>
+        public IntegerUpDownInspector()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
