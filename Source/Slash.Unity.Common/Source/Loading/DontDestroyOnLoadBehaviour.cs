7da762d9b477b25c723fb986260ecea0e530c93b
Christian Oeing <christian.oeing@slashgames.org>
2014-01-07 10:51:09 +0100

    ADDED: Unity.Common - Added Utility mono behaviours DontDestroyOnLoadBehaviour and LoadSceneBehaviour.

---
+++ Source/Slash.Unity.Common/Source/Loading/DontDestroyOnLoadBehaviour.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DontDestroyOnLoadBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Loading
+{
+    using UnityEngine;
+
+    /// <summary>
+    ///   The method DontDestroyOnLoad preserves the specified game object when
+    ///   the scene is changed.
+    ///   You can use it for game objects which are required throughout the game.
+    /// </summary>
+    public class DontDestroyOnLoadBehaviour : MonoBehaviour
+    {
+        #region Public Methods and Operators
+
+        public void Start()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Unity.Common/Source/Loading/LoadSceneBehaviour.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoadSceneBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Loading
+{
+    using UnityEngine;
+
+    public class LoadSceneBehaviour : MonoBehaviour
+    {
+        #region Fields
+
+        /// <summary>
+        ///   Indicates if the scene should be loaded additive to existing one.
+        ///   If loaded additive the old scene objects are not removed.
+        /// </summary>
+        public bool LoadAdditive;
+
+        /// <summary>
+        ///   Name of scene to load.
+        /// </summary>
+        public string SceneName;
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            if (this.LoadAdditive)
+            {
+                Application.LoadLevelAdditive(this.SceneName);
+            }
+            else
+            {
+                Application.LoadLevel(this.SceneName);
+            }
+        }
+
+        #endregion
+    }
+}
