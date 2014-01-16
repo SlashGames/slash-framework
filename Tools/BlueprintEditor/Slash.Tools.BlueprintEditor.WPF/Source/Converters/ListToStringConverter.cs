e2c7409753cc6412bc2ffaf2030cd6b5bb03933c
Nick Pruehs <dev@npruehs.de>
2014-01-16 15:25:36 +0100

    ADDED: Inspector - List inspector now shows "(none)" string for empty lists instead of nothing.

---
+++ Tools/BlueprintEditor/Slash.Tools.BlueprintEditor.WPF/Source/Converters/ListToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ListToStringConverter.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BlueprintEditor.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ListToStringConverter : IValueConverter
+    {
+        #region Public Methods and Operators
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = (string)value;
+            return string.IsNullOrWhiteSpace(str) ? "(none)" : str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
