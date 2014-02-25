3587397eaf3c02a9c71f012148371ca0aac9d909
Nick Pruehs <dev@npruehs.de>
2014-02-25 17:20:07 +0100

    ADDED: Serialization - Binary serializer for primitive types.

---
+++ Source/Slash.Serialization.Tests/Source/Binary/BinarySerializerTest.cs
@@ -0,0 +1,138 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BinarySerializerTest.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Serialization.Tests.Source.Binary
+{
+    using System.IO;
+
+    using NUnit.Framework;
+
+    using Slash.Serialization.Binary;
+
+    public class BinarySerializerTest
+    {
+        #region Fields
+
+        private BinarySerializer binarySerializer;
+
+        private MemoryStream memoryStream;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.binarySerializer = new BinarySerializer();
+            this.memoryStream = new MemoryStream();
+        }
+
+        [Test]
+        public void TestSerializeBool()
+        {
+            const bool B = true;
+            this.AssertSerializable(B);
+        }
+
+        [Test]
+        public void TestSerializeByte()
+        {
+            const byte B = 42;
+            this.AssertSerializable(B);
+        }
+
+        [Test]
+        public void TestSerializeChar()
+        {
+            const char C = '.';
+            this.AssertSerializable(C);
+        }
+
+        [Test]
+        public void TestSerializeDouble()
+        {
+            const double D = 42;
+            this.AssertSerializable(D);
+        }
+
+        [Test]
+        public void TestSerializeFloat()
+        {
+            const float F = 42f;
+            this.AssertSerializable(F);
+        }
+
+        [Test]
+        public void TestSerializeInt()
+        {
+            const int I = 42;
+            this.AssertSerializable(I);
+        }
+
+        [Test]
+        public void TestSerializeLong()
+        {
+            const long L = 42L;
+            this.AssertSerializable(L);
+        }
+
+        [Test]
+        public void TestSerializeSByte()
+        {
+            const sbyte B = 42;
+            this.AssertSerializable(B);
+        }
+
+        [Test]
+        public void TestSerializeShort()
+        {
+            const short S = 42;
+            this.AssertSerializable(S);
+        }
+
+        [Test]
+        public void TestSerializeString()
+        {
+            const string S = "Test";
+            this.AssertSerializable(S);
+        }
+
+        [Test]
+        public void TestSerializeUInt()
+        {
+            const uint I = 42;
+            this.AssertSerializable(I);
+        }
+
+        [Test]
+        public void TestSerializeULong()
+        {
+            const ulong L = 42;
+            this.AssertSerializable(L);
+        }
+
+        [Test]
+        public void TestSerializeUShort()
+        {
+            const ushort S = 42;
+            this.AssertSerializable(S);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AssertSerializable<T>(T o)
+        {
+            this.binarySerializer.Serialize(this.memoryStream, o);
+            this.memoryStream.Seek(0, SeekOrigin.Begin);
+            Assert.AreEqual(o, this.binarySerializer.Deserialize<T>(this.memoryStream));
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Serialization/Source/Binary/BinarySerializer.cs
@@ -0,0 +1,186 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BinarySerializer.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Serialization.Binary
+{
+    using System;
+    using System.IO;
+
+    public class BinarySerializer
+    {
+        #region Public Methods and Operators
+
+        public T Deserialize<T>(Stream stream)
+        {
+            Type type = typeof(T);
+            return (T)this.Deserialize(type, stream);
+        }
+
+        public object Deserialize(Type type, Stream stream)
+        {
+            BinaryReader reader = new BinaryReader(stream);
+            return this.Deserialize(type, reader);
+        }
+
+        public void Serialize(Stream stream, object o)
+        {
+            BinaryWriter writer = new BinaryWriter(stream);
+            Type type = o.GetType();
+
+            // Check for primitive types.
+            if (type.IsPrimitive)
+            {
+                if (o is bool)
+                {
+                    writer.Write((bool)o);
+                }
+                else if (o is byte)
+                {
+                    writer.Write((byte)o);
+                }
+                else if (o is char)
+                {
+                    writer.Write((char)o);
+                }
+                else if (o is double)
+                {
+                    writer.Write((double)o);
+                }
+                else if (o is float)
+                {
+                    writer.Write((float)o);
+                }
+                else if (o is int)
+                {
+                    writer.Write((int)o);
+                }
+                else if (o is long)
+                {
+                    writer.Write((long)o);
+                }
+                else if (o is sbyte)
+                {
+                    writer.Write((sbyte)o);
+                }
+                else if (o is short)
+                {
+                    writer.Write((short)o);
+                }
+                else if (o is uint)
+                {
+                    writer.Write((uint)o);
+                }
+                else if (o is ulong)
+                {
+                    writer.Write((ulong)o);
+                }
+                else if (o is ushort)
+                {
+                    writer.Write((ushort)o);
+                }
+                else
+                {
+                    throw new ArgumentException(string.Format("Unsupported primitive type: {0}", type.Name));
+                }
+
+                return;
+            }
+
+            // Check for string.
+            string s = o as string;
+
+            if (s != null)
+            {
+                writer.Write(s);
+                return;
+            }
+
+            throw new ArgumentException(string.Format("Unsupported type: {0}", type.Name));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private object Deserialize(Type type, BinaryReader reader)
+        {
+            if (type.IsPrimitive)
+            {
+                if (type == typeof(bool))
+                {
+                    return reader.ReadBoolean();
+                }
+
+                if (type == typeof(byte))
+                {
+                    return reader.ReadByte();
+                }
+
+                if (type == typeof(char))
+                {
+                    return reader.ReadChar();
+                }
+
+                if (type == typeof(double))
+                {
+                    return reader.ReadDouble();
+                }
+
+                if (type == typeof(float))
+                {
+                    return reader.ReadSingle();
+                }
+
+                if (type == typeof(int))
+                {
+                    return reader.ReadInt32();
+                }
+
+                if (type == typeof(long))
+                {
+                    return reader.ReadInt64();
+                }
+
+                if (type == typeof(sbyte))
+                {
+                    return reader.ReadSByte();
+                }
+
+                if (type == typeof(short))
+                {
+                    return reader.ReadInt16();
+                }
+
+                if (type == typeof(uint))
+                {
+                    return reader.ReadUInt32();
+                }
+
+                if (type == typeof(ulong))
+                {
+                    return reader.ReadUInt64();
+                }
+
+                if (type == typeof(ushort))
+                {
+                    return reader.ReadUInt16();
+                }
+
+                throw new ArgumentException(string.Format("Unsupported primitive type: {0}", type.Name));
+            }
+
+            // Check for string.
+            if (type == typeof(string))
+            {
+                return reader.ReadString();
+            }
+
+            throw new ArgumentException(string.Format("Unsupported type: {0}", type.Name));
+        }
+
+        #endregion
+    }
+}
