e3272091e9b1689f9140e1fbdfd8bb3f890db3dc
Christian Oeing <christian.oeing@slashgames.org>
2014-03-07 13:57:59 +0100

    ADDED: Unity.Common - Added generic scene manager to use for scene transitions.

---
+++ Source/Slash.Unity.Common/Source/Scenes/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿namespace Slash.Unity.Common.Scenes
+{
+    using UnityEngine;
+
+    public class SceneManager : MonoBehaviour
+    {
+        public object InitParams { get; set; }
+
+        public void LoadScene(string scene, object initParams)
+        {
+            this.InitParams = initParams;
+
+            Application.LoadLevel(scene);
+        }
+    }
+}
