e7c2e7311cfa3cce88360c25c6d45e03a5a56bfd
Nick Pruehs <dev@npruehs.de>
2014-03-14 12:10:10 +0100

    CHANGED: Delegates - Moved delegate creation code out of Unity Commons library, as it requires platform-dependent compilation. Use Slash.Reflection for creating delegate in logic, or platform-dependent compilation in Unity when required in MonoBehaviours.

---
+++ Source/Slash.Reflection.WindowsStore/Source/Utils/DelegateUtils.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DelegateUtils.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Reflection.Utils
+{
+    using System;
+    using System.Reflection;
+
+    public static class DelegateUtils
+    {
+        #region Public Methods and Operators
+
+        public static Delegate CreateDelegate(Type type, object target, string method)
+        {
+            var methodInfo = target.GetType().GetTypeInfo().GetDeclaredMethod(method);
+            return methodInfo.CreateDelegate(type);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Reflection/Source/Utils/DelegateUtils.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DelegateUtils.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Reflection.Utils
+{
+    using System;
+
+    public static class DelegateUtils
+    {
+        #region Public Methods and Operators
+
+        public static Delegate CreateDelegate(Type type, object target, string method)
+        {
+            return Delegate.CreateDelegate(type, target, method);
+        }
+
+        #endregion
+    }
+}
