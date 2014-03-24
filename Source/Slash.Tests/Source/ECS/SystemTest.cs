7e280d98454c28f620ed765e4ee6d0aebafa7da1
Christian Oeing <christian.oeing@slashgames.org>
2014-03-24 13:50:28 +0100

    ADDED: Tests - Added basic SystemTest to test systems more easily.

---
+++ Source/Slash.Tests/Source/ECS/SystemTest.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SystemTest.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Tests.ECS
+{
+    using NUnit.Framework;
+
+    using Slash.GameBase;
+    using Slash.GameBase.Systems;
+
+    public class SystemTest<T>
+        where T : ISystem, new()
+    {
+        #region Fields
+
+        protected Game TestGame;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            this.TestGame = new Game();
+
+            this.TestGame.AddSystem<T>();
+
+            this.TestGame.InitGame();
+            this.TestGame.StartGame(null);
+        }
+
+        #endregion
+    }
+}
