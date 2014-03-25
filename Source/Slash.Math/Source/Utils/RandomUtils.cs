5a4f1b38b8ce74c26fc25e0c5ad32a0ee72dc75d
Christian Oeing <christian.oeing@slashgames.org>
2014-03-25 13:01:57 +0100

    ADDED: Math - Added utility class for randomness related methods.

---
+++ Source/Slash.Math/Source/Utils/RandomUtils.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RandomUtils.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Math.Utils
+{
+    using System;
+
+    /// <summary>
+    ///   Utility methods which have to do with randomness.
+    /// </summary>
+    public static class RandomUtils
+    {
+        #region Static Fields
+
+        /// <summary>
+        ///   Random number generator.
+        /// </summary>
+        private static Random random;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///   Random number generator singleton.
+        /// </summary>
+        private static Random Random
+        {
+            get
+            {
+                return random ?? (random = new Random());
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Generates a random sign.
+        /// </summary>
+        /// <returns>-1 or 1.</returns>
+        public static int RandomSign()
+        {
+            return Random.NextDouble() < .5 ? 1 : -1;
+        }
+
+        /// <summary>
+        ///   Returns a random double number between the specified minimum and maximum (inclusive).
+        /// </summary>
+        /// <param name="min">Minimum value.</param>
+        /// <param name="max">Maximum value (inclusive).</param>
+        /// <returns>Random double number between the specified minimum and maximum (inclusive).</returns>
+        public static double RangeDouble(double min, double max)
+        {
+            if (min > max)
+            {
+                throw new ArgumentException(
+                    string.Format("Minium value ({0}) has to be less or equal maximum value ({1})", min, max), "min");
+            }
+
+            double range = max - min;
+            return min + Random.NextDouble() * range;
+        }
+
+        #endregion
+    }
+}
