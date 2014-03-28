2b2e2292b7a4acf48e3aec6e3f7c4bab0cd0eda5
Christian Oeing <christian.oeing@slashgames.org>
2014-03-28 14:31:07 +0100

    ADDED: Unity.Common - Added base behaviour to handle game events. ADDED: Unity.Common - Added extension methods for animations to wait till a specific or all animations are over.

---
+++ Source/Slash.Unity.Common/Source/Animations/AnimationExtensions.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AnimationExtensions.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Animations
+{
+    using System.Collections;
+
+    using UnityEngine;
+
+    public static class AnimationExtensions
+    {
+        #region Public Methods and Operators
+
+        public static IEnumerator WhilePlaying(this Animation animation)
+        {
+            do
+            {
+                yield return 0;
+            }
+            while (animation.isPlaying);
+        }
+
+        public static IEnumerator WhilePlaying(this Animation animation, string animationName)
+        {
+            while (animation.IsPlaying(animationName))
+            {
+                yield return 0;
+            }
+        }
+
+        #endregion
+    }
+}
