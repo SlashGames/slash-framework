b6291f3e7eac8105b80a9afec72a34bf5905e8e0
Christian Oeing <christian.oeing@slashgames.org>
2014-03-29 02:11:48 +0100

    ADDED: GameBase - Added optional parameter to CreateEntity method (extension method for game).

--- Source/Slash.GameBase/Source/Utils/GameBlueprintUtils.cs
+++ Source/Slash.GameBase/Source/Utils/GameBlueprintUtils.cs
@@ -9,6 +9,8 @@ namespace Slash.GameBase.Utils
     using System.Collections.Generic;
     using System.Linq;
 
+    using Slash.Collections.AttributeTables;
+
     /// <summary>
     ///   Utility methods for all issues related to blueprints of a game (e.g. entity creation,...).
     /// </summary>
@@ -39,8 +41,22 @@ namespace Slash.GameBase.Utils
         /// <returns>Id of created entity.</returns>
         public static int CreateEntity(this Game game, string blueprintId)
         {
+            return CreateEntity(game, blueprintId, null);
+        }
+
+        /// <summary>
+        ///   Searches for the blueprint with the specified id and creates an entity out of it.
+        ///   If the blueprint is used several times, consider to fetch the blueprint from the game's
+        ///   blueprint manager once and use it to create the entities.
+        /// </summary>
+        /// <param name="game">Game to get the blueprint from and create the entity in.</param>
+        /// <param name="blueprintId">Id of blueprint to use.</param>
+        /// <param name="configuration">Configuration to initialize the entity from.</param>
+        /// <returns>Id of created entity.</returns>
+        public static int CreateEntity(this Game game, string blueprintId, AttributeTable configuration)
+        {
             var blueprint = game.BlueprintManager.GetBlueprint(blueprintId);
-            return game.EntityManager.CreateEntity(blueprint);
+            return game.EntityManager.CreateEntity(blueprint, configuration);
         }
 
         #endregion
