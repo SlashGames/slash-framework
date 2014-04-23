01dd930d108271b6b913ac6bb458c2b2cf41d490
Christian Oeing <christian.oeing@slashgames.org>
2014-04-23 15:06:09 +0200

    ADDED: Unity.Editor.Common - Added mesh renderer editor to show sorting layer/order. Added EditorGUIUtils which contains utility methods e.g. to draw an inspector field for an array.

---
+++ Source/Slash.Unity.Editor.Common/Source/Inspectors/Rendering/MeshRendererSortingEditor.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MeshRendererSortingEditor.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.Inspectors.Rendering
+{
+    using UnityEditor;
+
+    using UnityEngine;
+
+    /// <summary>
+    ///   Adds sorting layer and order to mesh renderer to adjust it to work correctly with sprite rendering.
+    /// </summary>
+    [CustomEditor(typeof(MeshRenderer))]
+    public class MeshRendererSortingEditor : Editor
+    {
+        #region Public Methods and Operators
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            MeshRenderer renderer = (MeshRenderer)this.target;
+            if (renderer == null)
+            {
+                EditorGUILayout.HelpBox(
+                    "No mesh renderer found. Can't adjust sorting layer and order.", MessageType.Warning);
+                return;
+            }
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUI.BeginChangeCheck();
+
+            string sortingLayerName = EditorGUILayout.TextField("Sorting Layer Name", renderer.sortingLayerName);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                renderer.sortingLayerName = sortingLayerName;
+            }
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUI.BeginChangeCheck();
+
+            int order = EditorGUILayout.IntField("Sorting Order", renderer.sortingOrder);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                renderer.sortingOrder = order;
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        #endregion
+    }
+}
