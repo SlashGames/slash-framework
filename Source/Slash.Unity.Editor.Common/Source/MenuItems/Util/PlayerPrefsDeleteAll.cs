7cf98727d47bc20310d1c1e4fa43d6d32ffae2ac
Christian Oeing <christian.oeing@slashgames.org>
2014-05-29 17:09:27 +0200

    ADDED: Unity.Editor - Added utility menu item to delete all player prefs.

---
+++ Source/Slash.Unity.Editor.Common/Source/MenuItems/Util/PlayerPrefsDeleteAll.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PlayerPrefsDeleteAll.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.MenuItems.Util
+{
+    using UnityEditor;
+
+    using UnityEngine;
+
+    /// <summary>
+    ///   Finds prefabs who have the specified MonoBehaviour attached.
+    /// </summary>
+    public static class PlayerPrefsDeleteAll
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Deletes all player prefs.
+        /// </summary>
+        [MenuItem("Slash Games/Util/Delete All Player Prefs")]
+        public static void DeleteAllPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("Player preferences deleted.");
+        }
+
+        #endregion
+    }
+}
