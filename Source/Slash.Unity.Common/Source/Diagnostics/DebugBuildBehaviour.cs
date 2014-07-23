8b3bcc738decc0688a4dabe611d2c61deffb6c30
Christian Oeing <christian.oeing@slashgames.org>
2014-07-23 18:35:00 +0200

    ADDED: Unity.Common - Added behaviour which disables the game object if no in debug build.

---
+++ Source/Slash.Unity.Common/Source/Diagnostics/DebugBuildBehaviour.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DebugBuildBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Diagnostics
+{
+    using UnityEngine;
+
+    /// <summary>
+    ///   Disables the game object if not in debug build.
+    /// </summary>
+    public class DebugBuildBehaviour : MonoBehaviour
+    {
+        #region Methods
+
+        private void Awake()
+        {
+            this.gameObject.SetActive(Debug.isDebugBuild);
+        }
+
+        #endregion
+    }
+}
