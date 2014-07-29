1c4c5f227560e5f852a6857e327eb21f69123719
Christian Oeing <christian.oeing@slashgames.org>
2014-07-29 17:38:03 +0200

    ADDED: Unity.Common - Added behaviour to set Screen.sleepTimeout setting on startup.

---
+++ Source/Slash.Unity.Common/Source/GUI/SleepTimeoutBehaviour.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SleepTimeoutBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.GUI
+{
+    using UnityEngine;
+
+    /// <summary>
+    ///   Sets the Screen.sleepTimeout setting.
+    /// </summary>
+    public class SleepTimeoutBehaviour : MonoBehaviour
+    {
+        #region Fields
+
+        /// <summary>
+        ///   Manual defined timeout (in s).
+        ///   Only used if Setting is set to ManualDefined.
+        /// </summary>
+        public int DefinedTimeout;
+
+        /// <summary>
+        ///   Setting of sleep timeout.
+        /// </summary>
+        public AvailableSetting Setting;
+
+        #endregion
+
+        #region Enums
+
+        public enum AvailableSetting
+        {
+            SystemSetting,
+
+            NeverSleep,
+
+            ManualDefined
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Awake()
+        {
+            // Set sleep timeout.
+            int sleepTimeout;
+            switch (this.Setting)
+            {
+                case AvailableSetting.ManualDefined:
+                    sleepTimeout = this.DefinedTimeout;
+                    break;
+                case AvailableSetting.NeverSleep:
+                    sleepTimeout = SleepTimeout.NeverSleep;
+                    break;
+                default:
+                    sleepTimeout = SleepTimeout.SystemSetting;
+                    break;
+            }
+
+            Screen.sleepTimeout = sleepTimeout;
+        }
+        
+        #endregion
+    }
+}
