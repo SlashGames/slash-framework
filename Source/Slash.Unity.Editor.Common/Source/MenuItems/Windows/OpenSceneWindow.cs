f0182558930d8a80ceb508283e15367adcc815a4
Nick Prühs <dev@npruehs.de>
2014-08-19 18:15:23 +0200

    ADDED: Unity.Editor.Common - Generic scene menu.

---
+++ Source/Slash.Unity.Editor.Common/Source/MenuItems/Windows/OpenSceneWindow.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpenSceneWindow.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.MenuItems.Windows
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    using UnityEditor;
+
+    using UnityEngine;
+
+    public class OpenSceneWindow : EditorWindow
+    {
+        #region Static Fields
+
+        /// <summary>
+        ///   Asset folders to ignore when building the scene list.
+        /// </summary>
+        private static readonly string[] IgnoredFolders = new[] { "NGUI", "NData" };
+
+        /// <summary>
+        ///   Scenes accessible from the Open Scene window.
+        /// </summary>
+        private static IEnumerable<FileInfo> gameScenes;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [MenuItem("Slash Games/Windows/Open Scene")]
+        public static void OpenScene()
+        {
+            GetWindow(typeof(OpenSceneWindow), false, "Open Scene").Show();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void OnGUI()
+        {
+            if (gameScenes == null)
+            {
+                // Search all non-ignored directories for scene files.
+                DirectoryInfo directory = new DirectoryInfo(Application.dataPath);
+                FileInfo[] sceneFiles = directory.GetFiles("*.unity", SearchOption.AllDirectories);
+                gameScenes =
+                    sceneFiles.Where(
+                        file => IgnoredFolders.All(ignoredFolder => !file.FullName.Contains(ignoredFolder)));
+            }
+
+            foreach (var gameScene in gameScenes)
+            {
+                if (GUILayout.Button(gameScene.Name.Substring(0, gameScene.Name.IndexOf('.'))))
+                {
+                    EditorApplication.OpenScene(gameScene.FullName);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
