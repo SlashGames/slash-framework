66313f57e4c7f25001542fda2c379345259187e4
Nick Prühs <dev@npruehs.de>
2014-08-21 18:53:01 +0200

    ADDED: Unity.Common - Reusable build scripts of FreudBot.

---
+++ Source/Slash.Unity.Editor.Common/Source/MenuItems/Build/BuildType.cs
@@ -0,0 +1,17 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BuildType.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.Build
+{
+    public enum BuildType
+    {
+        None,
+
+        Debug,
+
+        Release,
+    }
+}
