79d3cdf0626fccbdcc30618d0309fe8301480e68
Christian Oeing <christian.oeing@slashgames.org>
2014-11-21 21:28:34 +0100

    ADDED: Slash.Unity - Added UnityVS project for UnityVS extensions.

---
+++ Source/Slash.Unity.UnityVS/Source/Unity/Assets/UnityVSExt/Editor/UnityVSUtils.cs
@@ -0,0 +1,20 @@
+﻿namespace Slash.Unity.UnityVSExt.Editor
+{
+    using SyntaxTree.VisualStudio.Unity.Bridge;
+
+    public class UnityVSUtils
+    {
+        #region Public Methods and Operators
+
+        public static FileGenerationHandler AdjustRootNamespace(string rootNamespace)
+        {
+            return
+                (name, content) =>
+                content.Replace(
+                    "<RootNamespace></RootNamespace>",
+                    string.Format("<RootNamespace>{0}</RootNamespace>", rootNamespace));
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Unity.UnityVS/Tools/MAKE.bat
@@ -0,0 +1,1 @@
+"%UNITY_PATH%Unity.exe" -batchmode -projectPath %~dp0..\Source\Unity -exportPackage Assets/UnityVSExt %~dp0..\Release\Slash.Unity.UnityVSExt.unitypackage -quit
