4a69e467f2e82c562143cff89b5d52212f0ac309
Christian Oeing <christian.oeing@slashgames.org>
2015-03-04 17:32:35 +0100

    ADDED: Unity.Common - Added WindowManager and LoadSceneBehaviour.

---
+++ Source/Slash.Unity.Common/Source/Loading/LoadSceneBehaviour.cs
@@ -0,0 +1,91 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoadSceneBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Loading
+{
+    using Slash.Unity.Common.Scenes;
+
+    using UnityEngine;
+
+    public class LoadSceneBehaviour : MonoBehaviour
+    {
+        #region Fields
+
+        /// <summary>
+        ///   Indicates if the scene should be loaded additive to existing one.
+        ///   If loaded additive the old scene objects are not removed.
+        /// </summary>
+        public bool LoadAdditive;
+
+        /// <summary>
+        ///   Scene manager to use. If not set, the levels are loaded directly.
+        /// </summary>
+        public SceneManager SceneManager;
+
+        /// <summary>
+        ///   Name of scene to load.
+        /// </summary>
+        public string SceneName;
+
+        /// <summary>
+        ///   Window manager to use. If not set, the windows are loaded directly.
+        /// </summary>
+        public WindowManager WindowManager;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void LoadScene()
+        {
+            if (this.WindowManager == null)
+            {
+                this.WindowManager = WindowManager.Instance;
+            }
+            if (this.SceneManager == null)
+            {
+                this.SceneManager = SceneManager.Instance;
+            }
+
+            if (this.LoadAdditive)
+            {
+                if (this.WindowManager != null)
+                {
+                    this.WindowManager.OpenWindow(this.SceneName);
+                }
+                else
+                {
+                    Application.LoadLevelAdditive(this.SceneName);
+                }
+            }
+            else
+            {
+                if (this.SceneManager != null)
+                {
+                    this.SceneManager.ChangeScene(this.SceneName);
+                }
+                else
+                {
+                    Application.LoadLevel(this.SceneName);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///   Unity callback.
+        /// </summary>
+        protected void Start()
+        {
+            this.LoadScene();
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Unity.Common/Source/Scenes/WindowRoot.cs
@@ -0,0 +1,40 @@
+﻿namespace Slash.Unity.Common.Scenes
+{
+    using System;
+
+    using UnityEngine;
+
+    public class WindowRoot : MonoBehaviour
+    {
+        #region Fields
+
+        public string WindowId;
+
+        #endregion
+
+        #region Public Events
+
+        public event Action<WindowRoot> WindowDestroyed;
+
+        #endregion
+
+        #region Methods
+
+        protected void OnDestroy()
+        {
+            Debug.Log(string.Format("Window root {0} is being destroyed, closing window.", this.WindowId));
+            this.OnWindowDestroyed();
+        }
+
+        private void OnWindowDestroyed()
+        {
+            var handler = this.WindowDestroyed;
+            if (handler != null)
+            {
+                handler(this);
+            }
+        }
+
+        #endregion
+    }
+}
