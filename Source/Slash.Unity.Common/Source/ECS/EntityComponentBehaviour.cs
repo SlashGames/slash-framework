4bbbfbe83fb27b177aa77d2c544768b6748a2457
Christian Oeing <christian.oeing@slashgames.org>
2015-05-10 16:29:09 +0200

    ADDED: Unity.Common - Added behaviour which works on a specific component of an entity.

---
+++ Source/Slash.Unity.Common/Source/ECS/EntityComponentBehaviour.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EntityComponentBehaviour.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.ECS
+{
+    using Slash.ECS.Components;
+
+    using UnityEngine;
+
+    public class EntityComponentBehaviour<T> : MonoBehaviour
+        where T : class, IEntityComponent
+    {
+        #region Fields
+
+        public EntityBehaviour Entity;
+
+        #endregion
+
+        #region Properties
+
+        protected T Component { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected void Start()
+        {
+            this.Component = this.Entity != null ? this.Entity.GetLogicComponent<T>() : null;
+        }
+
+        #endregion
+    }
+}
