d23ebe1a1f01bcc8557ea2f49edb0a85bd5ff296
Christian Oeing <christian.oeing@slashgames.org>
2015-07-26 15:02:48 +0200

    ADDED: Slash.Unity.DataBind.Ext - Added library to connect slash framework with data bind asset.

---
+++ Source/Slash.Unity.DataBind.Ext/Source/Windows/WindowContext.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WindowContext.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.DataBind.Ext.Windows
+{
+    using Slash.Unity.Common.Scenes;
+    using Slash.Unity.DataBind.Core.Data;
+
+    public class WindowContext : Context
+    {
+        #region Properties
+
+        /// <summary>
+        ///   Window which has this context.
+        /// </summary>
+        public WindowManager.Window Window { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected void Close(object returnValue = null)
+        {
+            WindowManager.Instance.CloseWindow(this.Window, returnValue);
+        }
+
+        #endregion
+    }
+}
