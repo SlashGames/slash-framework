0d6f91371d4ec27baf87045e29c5b76322736670
Christian Oeing <christian.oeing@slashgames.org>
2015-12-19 13:02:49 +0100

    ADDED: Unity.Common - Added DropDown script to use in user interface.

---
+++ Source/Slash.Unity.Common/Source/UI/DropDown.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DropDown.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.UI
+{
+    using UnityEngine;
+
+    public class DropDown : MonoBehaviour
+    {
+        #region Fields
+
+        public bool IsExpanded;
+
+        public RectTransform List;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public void SetExpand(bool isExpanded)
+        {
+            this.IsExpanded = isExpanded;
+            this.UpdateList();
+        }
+
+        public void ToggleExpand()
+        {
+            this.IsExpanded = !this.IsExpanded;
+            this.UpdateList();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected void Start()
+        {
+            this.UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            if (this.List != null)
+            {
+                this.List.gameObject.SetActive(this.IsExpanded);
+            }
+        }
+
+        #endregion
+    }
+}
