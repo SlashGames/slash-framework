3ba6dcc8fcb6a0f75e1612d2652131435097286b
Christian Oeing <christian.oeing@slashgames.org>
2015-12-19 13:02:08 +0100

    CHANGED: Unity.Common - Using SetParent instead of transform.parent to set parent in UnityGameObjectUtils.AddChild.

--- Source/Slash.Unity.Common/Source/Utils/UnityGameObjectUtils.cs
+++ Source/Slash.Unity.Common/Source/Utils/UnityGameObjectUtils.cs
@@ -50,7 +50,7 @@ namespace Slash.Unity.Common.Utils
             if (go != null && parent != null)
             {
                 Transform t = go.transform;
-                t.parent = parent.transform;
+                t.SetParent(parent.transform);
                 t.localPosition = Vector3.zero;
                 t.localRotation = Quaternion.identity;
                 t.localScale = Vector3.one;
