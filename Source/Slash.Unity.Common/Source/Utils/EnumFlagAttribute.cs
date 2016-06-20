8c2ecd5c3e365d8c129929e01a2af1978e9afd74
Christian Oeing <christian.oeing@slashgames.org>
2016-06-20 22:29:41 +0200

    ADDED: Unity.Common - Added enum flag attribute to get a a custom inspector for enum flags.

---
+++ Source/Slash.Unity.Common/Source/Utils/EnumFlagAttribute.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumFlagAttribute.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Common.Utils
+{
+    using UnityEngine;
+
+    public class EnumFlagAttribute : PropertyAttribute
+    {
+        #region Constructors and Destructors
+
+        public EnumFlagAttribute()
+        {
+        }
+
+        public EnumFlagAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Source/Slash.Unity.Editor.Common/Source/Utils/EnumFlagPropertyDrawer.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumFlagPropertyDrawer.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.Utils
+{
+    using System;
+
+    using Slash.Unity.Common.Utils;
+
+    using UnityEditor;
+
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(EnumFlagAttribute))]
+    public class EnumFlagDrawer : PropertyDrawer
+    {
+        #region Public Methods and Operators
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var flagSettings = (EnumFlagAttribute)this.attribute;
+            var targetEnum = GetBaseProperty<Enum>(property);
+
+            var propName = flagSettings.Name;
+            if (string.IsNullOrEmpty(propName))
+            {
+                propName = property.name;
+            }
+
+            EditorGUI.BeginProperty(position, label, property);
+            var enumNew = EditorGUI.EnumMaskField(position, propName, targetEnum);
+            var convertedType = Convert.ChangeType(enumNew, targetEnum.GetType());
+            if (convertedType != null)
+            {
+                property.intValue = (int)convertedType;
+            }
+            EditorGUI.EndProperty();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static T GetBaseProperty<T>(SerializedProperty prop)
+        {
+            // Separate the steps it takes to get to this property
+            var separatedPaths = prop.propertyPath.Split('.');
+
+            // Go down to the root of this serialized property
+            var reflectionTarget = prop.serializedObject.targetObject as object;
+            // Walk down the path to get the target object
+            foreach (var path in separatedPaths)
+            {
+                var fieldInfo = reflectionTarget.GetType().GetField(path);
+                reflectionTarget = fieldInfo.GetValue(reflectionTarget);
+            }
+            return (T)reflectionTarget;
+        }
+
+        #endregion
+    }
+}
