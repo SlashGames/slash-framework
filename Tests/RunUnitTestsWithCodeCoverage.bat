4a7a2b4439a71d4e335304ecc60b4df3f0c2ccd5
Christian Oeing <christian.oeing@slashgames.org>
2016-06-23 23:07:49 +0200

    CHANGED: Tests - Adjusted batch file to be closer to the command that is executed on build server.

--- Tests/RunUnitTestsWithCodeCoverage.bat
+++ Tests/RunUnitTestsWithCodeCoverage.bat
@@ -1,2 +1,9 @@
-..\Ext\OpenCover\OpenCover.Console.exe -target:RunUnitTests.bat -output:CodeCoverageResult.xml -coverbytest:*.Tests.dll -register:user
-..\Ext\ReportGenerator\bin\ReportGenerator.exe -reports:CodeCoverageResult.xml -targetdir:CodeCoverageReport
+REM Use local version of OpenCover if environment variable is not set
+if ["%OPEN_COVER_HOME%"]==[""] (
+  echo OPEN_COVER_HOME variable not set, using local version.
+  set OPEN_COVER_HOME=Ext\OpenCover\
+)
+
+cd ..
+"%OPEN_COVER_HOME%OpenCover.Console.exe" -target:"Ext\NUnit\bin\nunit3-console.exe" -targetargs:"Tests\UnitTests.nunit --result=UnitTestsResult.xml;format=nunit2" -output:"Tests/CodeCoverageResult.xml" -coverbytest:*.Tests.dll -register:user
+Ext\ReportGenerator\bin\ReportGenerator.exe -reports:"Tests/CodeCoverageResult.xml" -targetdir:"Tests/CodeCoverageReport"
