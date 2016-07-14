71328a3caae87b3b831512a9304b8ed080dd61a3
Christian Oeing <christian.oeing@slashgames.org>
2016-07-14 22:02:04 +0200

    ADDED: Unity.Editor.Common - Added tools to setup a new window scene.

---
+++ Source/Slash.Unity.Editor.Common/Source/UI/Windows/SetupWindowUtils.cs
@@ -0,0 +1,84 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SetupWindowUtils.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Slash.Unity.Editor.Common.UI.Windows
+{
+    using Slash.Unity.Common.Scenes;
+
+    using UnityEditor;
+    using UnityEditor.SceneManagement;
+    using UnityEditor.Sprites;
+
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    using SceneManager = UnityEngine.SceneManagement.SceneManager;
+
+    public static class SetupWindowUtils
+    {
+        [MenuItem("Slash Games/UI/Windows/Create Window")]
+        public static void CreateWindow()
+        {
+            EditorWindow.GetWindow<WindowSetupEditorWindow>(true, "Create New Window");
+        }
+        
+        public static GameObject CreateWindow(string windowId)
+        {
+            var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+
+            // Create window root.
+            var root = new GameObject(windowId);
+
+            var canvas = root.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            var canvasScaler = root.AddComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(1920, 1080);
+            canvasScaler.matchWidthOrHeight = 0.5f;
+            var graphicRaycaster = root.AddComponent<GraphicRaycaster>();
+
+            var windowRoot = root.AddComponent<WindowRoot>();
+            windowRoot.WindowId = windowId;
+
+            // Create UI node.
+            var ui = new GameObject("UI");
+            var uiRect = ui.AddComponent<RectTransform>();
+            ui.transform.SetParent(root.transform, false);
+            uiRect.anchorMin = Vector2.zero;
+            uiRect.anchorMax = Vector2.one;
+            uiRect.sizeDelta = Vector2.zero;
+            uiRect.anchoredPosition = Vector2.zero;
+
+            // Create window placeholder.
+            var windowPanel = new GameObject("Window");
+            windowPanel.transform.SetParent(ui.transform, false);
+            var windowRect = windowPanel.AddComponent<RectTransform>();
+            windowRect.anchorMin = Vector2.zero;
+            windowRect.anchorMax = Vector2.one;
+            windowRect.sizeDelta = new Vector2(-40, -40);
+            var windowImage = windowPanel.AddComponent<Image>();
+
+            // Create Logic node.
+            var logic = new GameObject("Logic");
+            logic.transform.SetParent(root.transform, false);
+
+            // Create Debug node.
+            var debug = new GameObject("Debug");
+            debug.transform.SetParent(root.transform, false);
+
+            var cameraGameObject = new GameObject("Camera");
+            cameraGameObject.AddComponent<Camera>();
+            cameraGameObject.transform.SetParent(debug.transform, false);
+
+            var eventSystem = new GameObject("EventSystem");
+            eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+            eventSystem.transform.SetParent(debug.transform, false);
+
+            return root;
+        }
+    }
+}
---
+++ Source/Slash.Unity.Editor.Common/Source/UI/Windows/WindowSetupEditorWindow.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WindowSetupEditorWindow.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Slash.Unity.Editor.Common.UI.Windows
+{
+    using UnityEditor;
+
+    using UnityEngine;
+
+    public class WindowSetupEditorWindow : EditorWindow
+    {
+        #region Fields
+
+        private string windowId = "NewWindow";
+
+        #endregion
+
+        #region Methods
+
+        protected void OnGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Window Id:", GUILayout.Width(100));
+            this.windowId = GUILayout.TextField(this.windowId);
+            GUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Create Window"))
+            {
+                var windowRoot = SetupWindowUtils.CreateWindow(this.windowId);
+                Selection.activeGameObject = windowRoot;
+                this.Close();
+            }
+        }
+
+        #endregion
+    }
+}
