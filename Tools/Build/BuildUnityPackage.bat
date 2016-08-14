2934a4ec0e2db2853ca7cffae9d996b56c4ce5ef
Christian Oeing <christian.oeing@slashgames.org>
2016-08-14 19:04:13 +0200

    Tools - Update BuildUnityPackage script to create a unity package automatically

--- Tools/Build/BuildUnityPackage.bat
+++ Tools/Build/BuildUnityPackage.bat
@@ -1,6 +1,11 @@
+REM Setup
+REM Install perl on your system, if not already existent: https://www.perl.org/get.html
+REM Set environment variables:
+REM NET40_HOME is the path to your local .NET Framework installation (e.g. C:\Windows\Microsoft.NET\Framework\v4.0.30319)
+REM UNITY_PATH is the path to your Unity3D installation (e.g. C:\Program Files\Unity)
+
 REM Clean and build solution
-REM NET40_HOME is the path to your local .NET Framework installation (e.g. C:\Windows\Microsoft.NET\Framework\v4.0.30319\)
-%NET40_HOME%MSBuild.exe ../../Source/Slash.Framework.sln /property:Configuration="Debug NoLog4Net" /p:Platform="Any CPU" /t:Clean;Build
+"%NET40_HOME%\MSBuild.exe" ../../Source/Slash.Framework.sln /property:Configuration="Debug NoLog4Net" /p:Platform="Any CPU" /t:Clean;Build
 
 REM Remove superfluous Unity.Editor.Common files.
 MD ..\..\Bin\Slash.Unity.Editor.Common\Debug\keep
@@ -14,4 +19,4 @@ perl BuildUnityPackage.pl "../../Bin/Slash.Unity.Common/AnyCPU/Debug NoLog4Net" 
 perl BuildUnityPackage.pl "../../Bin/Slash.Unity.Editor.Common/Debug" "Slash.Framework/Assets/Editor/Plugins"
 
 REM Build package
-"%UNITY_PATH%Unity.exe" -batchmode -projectPath %~dp0Slash.Framework -exportPackage Assets %~dp0Slash.Framework.unitypackage -quit
+"%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage Assets "%~dp0Slash.Framework.unitypackage" -quit
