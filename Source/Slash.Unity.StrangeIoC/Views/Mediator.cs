b478b6d0805e67c26a2bae87b313c0143ab36391
Christian Oeing <christian.oeing@slashgames.org>
2017-03-02 13:02:59 +0100

    Add base class for generic mediator for a view

---
+++ Source/Slash.Unity.StrangeIoC/Views/Mediator.cs
@@ -0,0 +1,10 @@
+﻿using strange.extensions.mediation.impl;
+
+namespace Slash.Unity.StrangeIoC.Views
+{
+    public abstract class Mediator<TView> : Mediator where TView : View
+    {
+        [Inject]
+        public TView View { get; set; }
+    }
+}
