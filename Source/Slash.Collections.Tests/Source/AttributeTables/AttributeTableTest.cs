f5e0ded84a3d4482ec7edd65e49f6980c5074707
Christian Oeing <christian.oeing@slashgames.org>
2017-03-14 17:52:27 +0100

    Slash.Collection - Add tests to serialize/deserialize attribute table with integer and Vector2 list

--- Source/Slash.Collections.Tests/Source/AttributeTables/AttributeTableTest.cs
+++ Source/Slash.Collections.Tests/Source/AttributeTables/AttributeTableTest.cs
@@ -7,6 +7,7 @@
 namespace Slash.Collections.Tests.Source.AttributeTables
 {
     using System;
+    using System.Collections.Generic;
 
     using NUnit.Framework;
 
@@ -26,16 +27,92 @@ namespace Slash.Collections.Tests.Source.AttributeTables
         [Test]
         public void TestSerialization()
         {
-            AttributeTable attributeTable = new AttributeTable { { "one", 1 } };
+            var attributeTable = new AttributeTable { { "one", 1 } };
             SerializationTestUtils.TestXmlSerialization(attributeTable);
         }
 
         [Test]
         public void TestSerializationNullValue()
         {
-            AttributeTable attributeTable = new AttributeTable { { "null", null } };
+            var attributeTable = new AttributeTable { { "null", null } };
             SerializationTestUtils.TestXmlSerialization(attributeTable);
         }
+
+        [Test]
+        public void TestSerializationListInt()
+        {
+            var attributeTable = new AttributeTable { { "ListInt", new List<int> { 1, 3, 2 } } };
+            SerializationTestUtils.TestXmlSerialization(attributeTable);
+        }
+
+        public struct Vector2
+        {
+            public float X;
+
+            public float Y;
+
+            /// <inheritdoc />
+            public bool Equals(Vector2 other)
+            {
+                return this.X.Equals(other.X) && this.Y.Equals(other.Y);
+            }
+
+            /// <inheritdoc />
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj))
+                {
+                    return false;
+                }
+                return obj is Vector2 && this.Equals((Vector2)obj);
+            }
+
+            /// <inheritdoc />
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return (this.X.GetHashCode() * 397) ^ this.Y.GetHashCode();
+                }
+            }
+        }
+
+        [Test]
+        public void TestSerializationListVector2()
+        {
+            var attributeTable = new AttributeTable
+            {
+                {
+                    "ListVector2",
+                    new List<Vector2>
+                    {
+                        new Vector2 { X = 1, Y = 2 },
+                        new Vector2 { X = 321, Y = 11 },
+                        new Vector2 { X = 52.23f, Y = 2323.23f }
+                    }
+                }
+            };
+            SerializationTestUtils.TestXmlSerialization(attributeTable);
+        }
+
+        // NOTE: Enable when dictionary serialization works out-of-the-box
+        //[Test]
+        //public void TestSerializationDictionaryStringFloat()
+        //{
+        //    var attributeTable = new AttributeTable
+        //    {
+        //        {
+        //            "DictionaryStringFloat",
+        //            new Dictionary<string, float>
+        //            {
+        //                { "One", 1.0f },
+        //                { "Pi", (float)Math.PI },
+        //                { "Answer To Everything", 42 }
+        //            }
+        //        }
+        //    };
+        //    SerializationTestUtils.TestXmlSerialization(attributeTable);
+        //}
 
         #endregion
     }
