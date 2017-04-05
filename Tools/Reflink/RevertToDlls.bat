ba5a7afb7878a8f8f10e57b160a1f095f37d74f2
Christian Oeing <christian.oeing@slashgames.org>
2017-04-05 14:43:00 +0200

    Tools.Reflink Adjust readme.txt, add RevertToDlls.bat to revert reflinks

---
+++ Tools/Reflink/RevertToDlls.bat
@@ -0,0 +1,22 @@
+set BATCH_DIR=%~dp0
+
+set UNITY_PATH=%BATCH_DIR%/../..
+
+REM Remove reflink to framework
+rmdir /s /q "%UNITY_PATH%/Assets/Slash.Framework"
+
+REM Restore dlls
+git checkout -- "%UNITY_PATH%/Assets/Plugins/Slash.*.dll"
+git checkout -- "%UNITY_PATH%/Assets/Plugins/Slash.*.dll.meta"
+git checkout -- "%UNITY_PATH%/Assets/Editor/Plugins/Slash.*.dll"
+git checkout -- "%UNITY_PATH%/Assets/Editor/Plugins/Slash.*.dll.meta"
+
+REM Restore Unity modules
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Slash.Unity.Common.meta"
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Slash.Unity.Common/*.*"
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Editor/Slash.Unity.Editor.Common.meta"
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Editor/Slash.Unity.Editor.Common/*.*"
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Slash.Unity.StrangeIoC.meta"
+git checkout -- "%UNITY_PATH%/Assets/Slash Framework/Slash.Unity.StrangeIoC/*.*"
+
+cd %BATCH_DIR%
--- Tools/Reflink/readme.txt
+++ Tools/Reflink/readme.txt
@@ -4,6 +4,9 @@ This makes sense if the framework should be edited during development.
 
 Usage:
 
-1. Copy batch files to project root (e.g. for Unity projects inside the Assets folder)
-2. Check the SetupFramework.bat and comment all libraries that you don't need
-3. Adjust your solution files to include the Slash libraries in the build (Unity does this automatically)
+1. Copy SetupFrameworkForProject.bat and SlashLibraries.txt files to your project
+2. Adjust SlashLibraries.txt to list all desired libraries
+3. Adjust SetupFrameworkForProject.bat: Set UNITY_PATH to point to your Unity project root (i.e. the folder with Assets, Libary, Temp,...)
+4. Make sure you have defined the SLASH_FRAMEWORK environment variable to point to your Slash Framework root folder
+5. Run the SetupFrameworkForProject.bat file
+6. Adjust your solution files to include the Slash libraries in the build (Unity does this automatically)
