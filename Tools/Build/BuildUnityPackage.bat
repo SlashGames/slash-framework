ec02a4a9b06d5be507dc6ce7413a3960beb9226d
Christian Oeing <christian.oeing@slashgames.org>
2017-04-11 18:25:08 +0200

    Tools.Build - Log less on the command line during building unity package

--- Tools/Build/BuildUnityPackage.bat
+++ Tools/Build/BuildUnityPackage.bat
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 
 REM Setup
 REM Install perl on your system, if not already existent: https://www.perl.org/get.html
@@ -15,15 +15,17 @@ if ["%UNITY_PATH%"] == [""] (
 	goto :EOF
 )
 
+echo Build libraries...
+
 REM Clean and build solution
-"%NET40_HOME%\MSBuild.exe" ../../Source/Slash.Framework.sln /property:Configuration="%CONFIG%" /p:Platform="Any CPU" /t:Clean;Build
+"%NET40_HOME%\MSBuild.exe" ../../Source/Slash.Framework.sln /verbosity:minimal /property:Configuration="%CONFIG%" /p:Platform="Any CPU" /t:Clean;Build > BuildLibraries.log
 
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
-  echo Build success
+  echo ...Build success
 )
 if not %BUILD_STATUS%==0 (
-  echo Build failed
+  echo ...Build failed with error code %BUILD_STATUS%
   goto :EOF
 )
 
@@ -33,16 +35,22 @@ rmdir /s /q Slash.Framework
 REM Remove Slash.Unity.Common.dll
 DEL /Q "..\..\Bin\Slash.Unity.Common\AnyCPU\%CONFIG%\Slash.Unity.Common.*"
 
+echo Copy dlls to package...
+
 REM Copy dlls from Unity.Common
-perl BuildUnityPackage.pl "../../Bin/Slash.Unity.Common/AnyCPU/%CONFIG%" "Slash.Framework/Assets/Slash Framework/Plugins"
+perl BuildUnityPackage.pl "../../Bin/Slash.Unity.Common/AnyCPU/%CONFIG%" "Slash.Framework/Assets/Slash Framework/Plugins" > CopyDlls.log
+
+echo Copy source files to package...
 
 REM Copy non-DLL files
-xcopy /h /s /i /y "../../Source/Slash.Unity.Common/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.Common"
-xcopy /h /s /i /y "../../Source/Slash.Unity.Editor.Common/Source" "Slash.Framework/Assets/Slash Framework/Editor/Slash.Unity.Editor.Common"
-xcopy /h /s /i /y "../../Ext/StrangeIoC/Source" "Slash.Framework/Assets/StrangeIoC"
-xcopy /h /s /i /y "../../Source/Slash.Unity.StrangeIoC/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC"
-xcopy /h /s /i /y "../../Source/Slash.Unity.StrangeIoC.Video/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC.Video"
-xcopy /h /s /y "../../Source/Slash.Unity.Export/Source/Assets" "Slash.Framework/Assets"
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.Common/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.Common" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.Editor.Common/Source" "Slash.Framework/Assets/Slash Framework/Editor/Slash.Unity.Editor.Common" > nul
+xcopy /h /s /q /i /y "../../Ext/StrangeIoC/Source" "Slash.Framework/Assets/StrangeIoC" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC.Video/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC.Video" > nul
+xcopy /h /s /q /y "../../Source/Slash.Unity.Export/Source/Assets" "Slash.Framework/Assets" > nul
+
+echo Build addon unity packages...
 
 REM Build addons
 mkdir "%~dp0Slash.Framework/Assets/Slash Framework/Addons"
@@ -51,6 +59,8 @@ mkdir "%~dp0Slash.Framework/Assets/Slash Framework/Addons"
 REM Remove addons
 rmdir /s /q "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC"
 rmdir /s /q "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC.Video"
+
+echo Build main unity package...
 
 REM Build package
 "%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage "Assets/Slash Framework" "%~dp0%PACKAGE%.unitypackage" -logFile Unity.log -quit
