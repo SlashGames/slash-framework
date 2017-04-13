104dd0ba0a510aef3e860c4a139c34724d554da0
Christian Oeing <christian.oeing@slashgames.org>
2017-04-13 12:53:16 +0200

    Tools.Reflink Remove obsolete slashes from batch file

--- Tools/Reflink/UseReflinkForProject.bat
+++ Tools/Reflink/UseReflinkForProject.bat
@@ -1,7 +1,7 @@
 set BATCH_DIR=%~dp0
 
 REM Adjust this path to point to Unity Project folder
-set UNITY_PROJECT_PATH=%BATCH_DIR%/../..
+set UNITY_PROJECT_PATH=%BATCH_DIR%../..
 
 REM Reflink framework
-call "%SLASH_FRAMEWORK%/Tools/Reflink/UseReflink.bat" "%SLASH_FRAMEWORK%" "%UNITY_PROJECT_PATH%" "%BATCH_DIR%/SlashLibraries.txt"
+call "%SLASH_FRAMEWORK%/Tools/Reflink/UseReflink.bat" "%SLASH_FRAMEWORK%" "%UNITY_PROJECT_PATH%" "%BATCH_DIR%SlashLibraries.txt"
