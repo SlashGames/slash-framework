1240fb3d67d48e8f8009651ee8c68a6b78e6ba50
Christian Oeing <christian.oeing@slashgames.org>
2017-04-20 17:18:36 +0200

    Unity.StrangeIoC Move Init/Deinit to base class StrangeBridge to make deriving a concrete class easier

--- Source/Slash.Unity.StrangeIoC/Source/Configs/StrangeBridge.cs
+++ Source/Slash.Unity.StrangeIoC/Source/Configs/StrangeBridge.cs
@@ -2,5 +2,30 @@
 {
     public abstract class StrangeBridge
     {
+        /// <summary>
+        ///   Called to initialize bridge.
+        /// </summary>
+        protected virtual void Init()
+        {
+        }
+
+        /// <summary>
+        ///   Called to deinitialize the bridge.
+        /// </summary>
+        protected virtual void Deinit()
+        {
+        }
+
+        [Deconstruct]
+        public void OnDeconstruct()
+        {
+            this.Deinit();
+        }
+
+        [PostConstruct]
+        public void OnPostConstruct()
+        {
+            this.Init();
+        }
     }
 }
