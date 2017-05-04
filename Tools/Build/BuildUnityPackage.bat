f7450b50ef53a23b870fd535bacae566a1676dae
Christian Oeing <christian.oeing@slashgames.org>
2017-05-04 21:29:37 +0200

    Issue #43 Change path for imported Unity package

--- Tools/Build/BuildUnityPackage.bat
+++ Tools/Build/BuildUnityPackage.bat
@@ -35,26 +35,28 @@ if not %BUILD_STATUS%==0 (
 REM Clear output path
 if exist Slash.Framework rmdir /s /q Slash.Framework
 
+set UNITY_SLASH_FRAMEWORK_PATH=Slash.Framework/Assets/SlashFramework
+
 echo Copy dlls to package...
 
 REM Copy dlls from Unity.Common
-perl BuildUnityPackage.pl "../../Bin" "%CONFIG%" "UnityPackageLibraries.txt" "Slash.Framework/Assets/Slash Framework/Plugins" > "logs/CopyDlls.log"
+perl BuildUnityPackage.pl "../../Bin" "%CONFIG%" "UnityPackageLibraries.txt" "%UNITY_SLASH_FRAMEWORK_PATH%/Plugins" > "logs/CopyDlls.log"
 
 echo Copy source files to package...
 
 REM Copy non-DLL files
-xcopy /h /s /q /i /y "../../Source/Slash.Unity.Common/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.Common" > nul
-xcopy /h /s /q /i /y "../../Source/Slash.Unity.Editor.Common/Source" "Slash.Framework/Assets/Slash Framework/Editor/Slash.Unity.Editor.Common" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.Common/Source" "%UNITY_SLASH_FRAMEWORK_PATH%/Slash.Unity.Common" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.Editor.Common/Source" "%UNITY_SLASH_FRAMEWORK_PATH%/Editor/Slash.Unity.Editor.Common" > nul
 xcopy /h /s /q /i /y "../../Ext/StrangeIoC/Source" "Slash.Framework/Assets/StrangeIoC" > nul
-xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC" > nul
-xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC.Video/Source" "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC.Video" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC/Source" "%UNITY_SLASH_FRAMEWORK_PATH%/Slash.Unity.StrangeIoC" > nul
+xcopy /h /s /q /i /y "../../Source/Slash.Unity.StrangeIoC.Video/Source" "%UNITY_SLASH_FRAMEWORK_PATH%/Slash.Unity.StrangeIoC.Video" > nul
 xcopy /h /s /q /y "../../Source/Slash.Unity.Export/Source/Assets" "Slash.Framework/Assets" > nul
 
 echo Build addon unity packages...
 
 REM Build addons
-mkdir "%~dp0Slash.Framework/Assets/Slash Framework/Addons"
-"%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage "Assets/Slash Framework/Slash.Unity.StrangeIoC" "Assets/Slash Framework/Slash.Unity.StrangeIoC.Video" "%~dp0Slash.Framework/Assets/Slash Framework/Addons/Slash.Unity.StrangeIoC.unitypackage" -logFile "logs/AddonStrangeIoC.log" -quit
+mkdir "%~dp0%UNITY_SLASH_FRAMEWORK_PATH%/Addons"
+"%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage "Assets/SlashFramework/Slash.Unity.StrangeIoC" "Assets/SlashFramework/Slash.Unity.StrangeIoC.Video" "%~dp0%UNITY_SLASH_FRAMEWORK_PATH%/Addons/Slash.Unity.StrangeIoC.unitypackage" -logFile "logs/AddonStrangeIoC.log" -quit
 
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
@@ -66,13 +68,13 @@ if not %BUILD_STATUS%==0 (
 )
 
 REM Remove addons
-rmdir /s /q "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC"
-rmdir /s /q "Slash.Framework/Assets/Slash Framework/Slash.Unity.StrangeIoC.Video"
+rmdir /s /q "%UNITY_SLASH_FRAMEWORK_PATH%/Slash.Unity.StrangeIoC"
+rmdir /s /q "%UNITY_SLASH_FRAMEWORK_PATH%/Slash.Unity.StrangeIoC.Video"
 
 echo Build main unity package...
 
 REM Build package
-"%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage "Assets/Slash Framework" "%~dp0%PACKAGE%.unitypackage" -logFile logs/MainPackage.log -quit
+"%UNITY_PATH%\Editor\Unity.exe" -batchmode -projectPath "%~dp0Slash.Framework" -exportPackage "Assets/SlashFramework" "%~dp0%PACKAGE%.unitypackage" -logFile logs/MainPackage.log -quit
 
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
