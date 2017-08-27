b538b2c1cddda1ab3766a00307cbb5edde9bebb2
Christian Oeing <christian.oeing@slashgames.org>
2017-08-27 15:23:16 +0200

    Add batch file for Unity 2017.1 package

---
+++ Tools/Build/BuildUnityPackage.2017.1.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Setup
+REM Install perl on your system, if not already existent: https://www.perl.org/get.html
+REM Set environment variables:
+REM UNITY_2017_1_PATH is the path to your Unity3D 2017.1 installation (e.g. C:\Program Files\Unity)
+
+if ["%UNITY_2017_1_PATH%"] == [""] (
+	echo UNITY_2017_1_PATH not set
+	goto :EOF
+)
+
+BuildUnityPackage.bat "%UNITY_2017_1_PATH%" "Debug" "Slash.Framework.1.X.Unity.2017.1" > logs/BuildUnity20171Package.log
