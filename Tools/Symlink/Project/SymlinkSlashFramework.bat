cd1677e9cf24fd950399734c04b1ccea57ccea4f
Christian Oeing <christian.oeing@slashgames.org>
2017-09-21 15:39:46 +0200

    Add variable to adjust in SymlinkSlashFramework.bat if Slash Framework was added at different path than Slash.Framework

--- Tools/Symlink/Project/SymlinkSlashFramework.bat
+++ Tools/Symlink/Project/SymlinkSlashFramework.bat
@@ -1,7 +1,10 @@
 set BATCH_DIR=%~dp0
 
 REM Adjust this path to point to Unity Project folder
-set SYMLINK_PATH=%BATCH_DIR%..\Assets\SlashFramework\
+set SYMLINK_PATH=%BATCH_DIR%../Assets/SlashFramework/
+
+REM Adjust this path to point to Slash Framework folder
+set SLASH_FRAMEWORK_PATH=%BATCH_DIR%../Slash.Framework/
 
 REM Symlink framework
-call "../Slash.Framework/Tools/Symlink/UseSymlink.bat" "%SYMLINK_PATH%" "%BATCH_DIR%SlashLibraries.txt"
+call "%SLASH_FRAMEWORK_PATH%Tools/Symlink/UseSymlink.bat" "%SYMLINK_PATH%" "%BATCH_DIR%SlashLibraries.txt"
