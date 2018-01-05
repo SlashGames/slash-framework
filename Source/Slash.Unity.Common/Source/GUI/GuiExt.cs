af9553d05b8c7c307004a0cd6d9cafffd219938a
Christian Oeing <christian.oeing@slashgames.org>
2018-01-05 12:56:22 +0100

    Move ECS and Application dependent scripts from Slash.Unity.Common to Slash.Unity.ECS

--- Source/Slash.Unity.Common/Source/GUI/GuiExt.cs
+++ Source/Slash.Unity.Common/Source/GUI/GuiExt.cs
@@ -55,14 +55,14 @@ namespace Slash.Unity.Common.GUI
             bool done = false;
             switch (Event.current.GetTypeForControl(controlID))
             {
-                case EventType.mouseDown:
+                case EventType.MouseDown:
                     if (position.Contains(Event.current.mousePosition))
                     {
                         GUIUtility.hotControl = controlID;
                         showList = true;
                     }
                     break;
-                case EventType.mouseUp:
+                case EventType.MouseUp:
                     if (showList)
                     {
                         done = true;
