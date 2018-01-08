04b5fd20cd65ca4b621f456656f3c5d85dca28c1
Christian Oeing <christian.oeing@slashgames.org>
2018-01-08 15:50:28 +0100

    Add utility method to create an asset for a ScriptableObject

---
+++ Source/Slash.Unity.Editor.Common/Source/ScriptableObjects/ScriptableObjectUtils.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Slash.Unity.Editor.Common.ScriptableObjects
+{
+    public static class ScriptableObjectUtils
+    {
+        /// <summary>
+        ///     This makes it easy to create, name and place unique new ScriptableObject asset files.
+        /// </summary>
+        /// <param name="defaultName">Default name of new asset. If not set it is called 'New TypeName.asset'.</param>
+        /// <returns>Returns the asset which was created.</returns>
+        public static T CreateAsset<T>(string defaultName = null) where T : ScriptableObject
+        {
+            var asset = ScriptableObject.CreateInstance<T>();
+
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (string.IsNullOrEmpty(path))
+            {
+                path = "Assets";
+            }
+            else if (Path.GetExtension(path) != "")
+            {
+                path = path.Replace(Path.GetFileName(path), "");
+            }
+
+            if (string.IsNullOrEmpty(defaultName))
+            {
+                defaultName = "New " + typeof(T).Name;
+            }
+
+            var assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + defaultName + ".asset");
+
+            AssetDatabase.CreateAsset(asset, assetPathAndName);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = asset;
+
+            return asset;
+        }
+    }
+}
