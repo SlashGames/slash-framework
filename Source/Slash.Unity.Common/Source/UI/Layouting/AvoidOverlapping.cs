aafc94719492ee047c92879e5efce4d44af7ab52
Christian Oeing <christian.oeing@slashgames.org>
2018-01-24 11:16:45 +0100

    Slash.Unity.Common - Add layout group to avoid vertical overlapping for child rectangles
    Layouting doesn't move the rectangles if not overlapping

---
+++ Source/Slash.Unity.Common/Source/UI/Layouting/AvoidOverlapping.cs
@@ -0,0 +1,90 @@
+﻿namespace Slash.Unity.Common.UI.Layouting
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public static class AvoidOverlapping
+    {
+        public static void DoVerticalLayout(List<LayoutRectangle> layoutRectangles, float space)
+        {
+            // Sort by Y position.
+            layoutRectangles.Sort(LayoutRectangle.SortByPositionYDesc);
+
+            for (var index = 0; index < layoutRectangles.Count - 1; index++)
+            {
+                var layoutRectangle = layoutRectangles[index];
+                var nextLayoutRectangle = layoutRectangles[index + 1];
+
+                // Check if overlaps with next rectangle.
+                var overlapDistance = nextLayoutRectangle.Top - layoutRectangle.Bottom + space;
+                if (overlapDistance > 0)
+                {
+                    // Move rectangle up as far as possible.
+                    var rectangleMove =
+                        index == 0 ? overlapDistance : Math.Min(layoutRectangle.FreeSpace, overlapDistance);
+                    layoutRectangle.FreeSpace -= rectangleMove;
+                    layoutRectangle.Move(0, rectangleMove);
+
+                    // Move next rectangle by remaining distance.
+                    var remainingMove = overlapDistance - rectangleMove;
+                    if (remainingMove > 0)
+                    {
+                        nextLayoutRectangle.Move(0, -remainingMove);
+                    }
+                }
+                else
+                {
+                    // Use distance as buffer for next rectangle to move.
+                    nextLayoutRectangle.FreeSpace = -overlapDistance;
+                }
+            }
+        }
+
+        /// <summary>
+        ///     A rectangle to do layout with.
+        /// </summary>
+        public class LayoutRectangle
+        {
+            /// <summary>
+            ///     Free space that's available for this layout rectangle.
+            /// </summary>
+            public float FreeSpace;
+
+            /// <summary>
+            ///   Custom user data.
+            /// </summary>
+            public object Data;
+
+            public float Bottom
+            {
+                get { return this.Position.y - this.Size.y; }
+            }
+
+            public Vector2 Center
+            {
+                get { return this.Position; }
+            }
+
+            public float Top
+            {
+                get { return this.Position.y; }
+            }
+
+            public Vector2 Position;
+
+            public Vector2 Size;
+
+            public void Move(float moveX, float moveY)
+            {
+                this.Position.x += moveX;
+                this.Position.y += moveY;
+            }
+
+            public static int SortByPositionYDesc(LayoutRectangle rectangleA, LayoutRectangle rectangleB)
+            {
+                return rectangleB.Top.CompareTo(rectangleA.Top);
+            }
+        }
+    }
+}
---
+++ Source/Slash.Unity.Common/Source/UI/Layouting/AvoidVerticalOverlapLayoutGroup.cs
@@ -0,0 +1,82 @@
+﻿namespace Slash.Unity.Common.UI.Layouting
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Vriction.Catalog.Ella.LookPriceTags.Views;
+
+    /// <summary>
+    ///     Does layouting for the child rectangles, so they don't overlap.
+    /// </summary>
+    public class AvoidVerticalOverlapLayoutGroup : LayoutGroup
+    {
+        /// <summary>
+        ///     Minimum space between each child rectangle (in pixels).
+        /// </summary>
+        [Tooltip("Minimum space between each child rectangle (in pixels)")]
+        public float Space;
+
+        /// <inheritdoc />
+        public override void CalculateLayoutInputVertical()
+        {
+            float totalMin = 0;
+            float totalPreferred = 0;
+            foreach (var rectChild in this.rectChildren)
+            {
+                totalMin += rectChild.sizeDelta.y;
+                totalPreferred += rectChild.sizeDelta.y + this.Space;
+            }
+
+            this.SetLayoutInputForAxis(totalMin, totalPreferred, totalPreferred, 1);
+        }
+
+        /// <inheritdoc />
+        public override void SetLayoutHorizontal()
+        {
+            this.DoLayout(0);
+        }
+
+        /// <inheritdoc />
+        [ContextMenu("Do Layout")]
+        public override void SetLayoutVertical()
+        {
+            this.DoLayout(1);
+        }
+
+        private void DoLayout(int axis)
+        {
+            if (this.rectChildren.Count == 0)
+            {
+                return;
+            }
+
+            // Collect child rectangles.
+            var layoutRectangles = new List<AvoidOverlapping.LayoutRectangle>();
+            foreach (var rectChild in this.rectChildren)
+            {
+                var autoLayoutRectangle = rectChild.GetComponent<AutoLayoutRectangle>();
+                if (autoLayoutRectangle == null)
+                {
+                    continue;
+                }
+
+                layoutRectangles.Add(new AvoidOverlapping.LayoutRectangle
+                {
+                    Data = rectChild,
+                    Position = autoLayoutRectangle.PreferredPosition,
+                    Size = rectChild.sizeDelta
+                });
+            }
+
+            // Do auto layout for children.
+            AvoidOverlapping.DoVerticalLayout(layoutRectangles, this.Space);
+
+            // Update rect transforms of layout rectangles.
+            foreach (var layoutRectangle in layoutRectangles)
+            {
+                var rectChild = (RectTransform) layoutRectangle.Data;
+                rectChild.anchoredPosition = layoutRectangle.Position;
+            }
+        }
+    }
+}
