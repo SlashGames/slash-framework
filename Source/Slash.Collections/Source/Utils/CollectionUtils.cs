b422c9fedb49a89f1d2b39019e7bdca6688113ba
Christian Oeing <christian.oeing@slashgames.org>
2018-01-26 12:40:30 +0100

    Slash.Collections - Add Implode utility method to convert enumerables to strings (for debugging mainly)

--- Source/Slash.Collections/Source/Utils/CollectionUtils.cs
+++ Source/Slash.Collections/Source/Utils/CollectionUtils.cs
@@ -74,6 +74,35 @@ namespace Slash.Collections.Utils
         }
 
         /// <summary>
+        ///   Creates a separated string from the items of a collection.
+        ///   Mostly used for debugging issues.
+        /// </summary>
+        /// <param name="collection">Collection of items to create a string from.</param>
+        /// <param name="separator">String to use to separate the item strings.</param>
+        /// <typeparam name="T">Type of items in collection.</typeparam>
+        /// <returns></returns>
+        public static string Implode<T>(this IEnumerable<T> collection, string separator)
+        {
+            return collection != null
+                ? string.Join(separator, collection.Select(item => item != null ? item.ToString() : "<null>").ToArray())
+                : string.Empty;
+        }
+
+        /// <summary>
+        ///   Creates a separated string from the items of a collection.
+        ///   Mostly used for debugging issues.
+        /// </summary>
+        /// <param name="collection">Collection of items to create a string from.</param>
+        /// <param name="separator">String to use to separate the item strings.</param>
+        /// <returns>String created from the items which are separated by the provided separator.</returns>
+        public static string Implode(this IEnumerable collection, string separator)
+        {
+            return collection != null
+                ? collection.Cast<object>().Implode(separator)
+                : string.Empty;
+        }
+
+        /// <summary>
         ///   Compares the two passed lists for equality. Two lists are considered equal, if they contain equal elements in the
         ///   same order.
         /// </summary>
