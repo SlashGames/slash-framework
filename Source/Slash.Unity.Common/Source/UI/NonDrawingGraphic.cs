774dc3ff3b21feac9044d4296c77eaffdef3f6c6
Christian Oeing <christian.oeing@slashgames.org>
2018-02-01 18:14:31 +0100

    Slash.Unity.Common - Add NonDrawingGraphic to use e.g. as click area which shouldn't be rendered, but react on raycasts

---
+++ Source/Slash.Unity.Common/Source/UI/NonDrawingGraphic.cs
@@ -0,0 +1,30 @@
+﻿namespace Slash.Unity.Common.UI
+{
+    using UnityEngine.UI;
+
+    /// <summary>
+    ///     A concrete subclass of the Unity UI `Graphic` class that just skips drawing.
+    ///     Useful for providing a raycast target without actually drawing anything.
+    ///     From https://answers.unity.com/questions/1091618/ui-panel-without-image-component-as-raycast-target.html
+    /// </summary>
+    public class NonDrawingGraphic : Graphic
+    {
+        /// <inheritdoc />
+        public override void SetMaterialDirty()
+        {
+        }
+
+        /// <inheritdoc />
+        public override void SetVerticesDirty()
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            // Probably not necessary since the chain of calls `Rebuild()`->`UpdateGeometry()`->`DoMeshGeneration()`->`OnPopulateMesh()` won't happen; 
+            // so here really just as a fail-safe.
+            vh.Clear();
+        }
+    }
+}
---
+++ Source/Slash.Unity.Editor.Common/Source/UI/NonDrawingGraphicEditor.cs
@@ -0,0 +1,21 @@
+﻿namespace Slash.Unity.Editor.Common.UI
+{
+    using Slash.Unity.Common.UI;
+    using UnityEditor;
+    using UnityEditor.UI;
+
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(NonDrawingGraphic), false)]
+    public class NonDrawingGraphicEditor : GraphicEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            this.serializedObject.Update();
+            EditorGUILayout.PropertyField(this.m_Script);
+
+            // skipping AppearanceControlsGUI
+            this.RaycastControlsGUI();
+            this.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
