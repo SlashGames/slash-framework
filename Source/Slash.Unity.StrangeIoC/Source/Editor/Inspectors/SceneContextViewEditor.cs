32917a1701842ac785ec28991c45e805468dcf3c
Christian Oeing <christian.oeing@slashgames.org>
2018-02-09 15:40:00 +0100

    Unity.StrangeIoC - Add custom editor for scene context view to show name of bound context

---
+++ Source/Slash.Unity.StrangeIoC/Source/Editor/Inspectors/SceneContextViewEditor.cs
@@ -0,0 +1,29 @@
+﻿namespace Slash.Unity.StrangeIoC.Editor.Inspectors
+{
+    using Slash.Unity.StrangeIoC.Modules;
+
+    using UnityEditor;
+
+    [CustomEditor(typeof(SceneContextView))]
+    public class SceneContextViewEditor : Editor
+    {
+        /// <inheritdoc />
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var contextView = this.target as SceneContextView;
+            if (contextView == null)
+            {
+                return;
+            }
+
+            // Show module information.
+            var moduleContext = contextView.context as ModuleContext;
+            if (moduleContext != null)
+            {
+                EditorGUILayout.TextField("Module", moduleContext.Name);
+            }
+        }
+    }
+}
