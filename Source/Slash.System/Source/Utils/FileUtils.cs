159efa70fa2729c58a036973b65d174d42f228c8
Christian Oeing <christian.oeing@slashgames.org>
2018-02-22 17:31:11 +0100

    Slash.System - Add utility method to copy a directory

---
+++ Source/Slash.System/Source/Utils/FileUtils.cs
@@ -0,0 +1,27 @@
+﻿namespace Slash.SystemExt.Utils
+{
+    using System.IO;
+
+    public static class FileUtils
+    {
+        public static void CopyDirectory(string sourcePath, string destinationPath)
+        {
+            sourcePath = Path.GetFullPath(sourcePath);
+            destinationPath = Path.GetFullPath(destinationPath);
+
+            Directory.CreateDirectory(destinationPath);
+
+            //Now Create all of the directories
+            foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, destinationPath));
+            }
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, destinationPath), true);
+            }
+        }
+    }
+}
